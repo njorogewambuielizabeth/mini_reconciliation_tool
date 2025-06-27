6c46b894eb1b6da16af916a6e48d7404bf33265b
Elizabeth Njoroge <njorogewambuielizabeth@gmail.com>
2025-06-27 14:18:50 +0300

    made changes to my dependancies

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',   
-    port: 5000, 
-    allowedHosts: 'all',       
+    host: '0.0.0.0',
+    port: 5000,
+    allowedHosts: [
+      'all',
+      'b11b8f3d-4905-49ce-84bd-4e16cae71379-00-23w6s44pz5cu1.kirk.replit.dev' // your Replit URL
+    ]
   }
 })
