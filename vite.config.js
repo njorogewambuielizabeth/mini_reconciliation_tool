f88d53bf38206038c33a8a780b042ff8f84f7e6d
Elizabeth Njoroge <njorogewambuielizabeth@gmail.com>
2025-06-27 14:09:34 +0300

    made changes to my dependancies

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',   // ← this exposes it to Replit
-    port: 5000        // ← default Vite port
+    host: '0.0.0.0',   
+    port: 5000, 
+    allowedHosts: 'all',       
   }
 })
